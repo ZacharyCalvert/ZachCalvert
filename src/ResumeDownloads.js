d8c999db7b0e68b3aac7094ce654cc1f749de2aa
Zach Calvert <ZachCalvert@gmail.com>
2016-12-02 20:00:08 -0600

    Adding download buttons

--- src/ResumeDownloads.js
+++ src/ResumeDownloads.js
@@ -7,13 +7,20 @@ class ResumeDownloads extends Component {
     return (
         <div className="container">
           <div className="row">
-          	<div className="col-sm-8 col-xs-12 center-block">
-              <ul className="list-inline">
-	              <li className="px-2"><a href="./resume/ZachCalvert_2pg.pdf" download="ZachCalvert.pdf">2pg-pdf</a></li>
-	              <li><a href="./resume/ZachCalvert_4pg.pdf"  download="ZachCalvert.pdf">4pg-pdf</a></li>
-	              <li><a href="./resume/ZachCalvert_2pg.docx"  download="ZachCalvert.docx">2pg-docx</a></li>
-	              <li><a href="./resume/ZachCalvert_4pg.docx"  download="ZachCalvert.docx">4pg-docx</a></li>
-              </ul>
+          	<div className="col-sm-8 col-xs-12">
+              <div className="col-sm-4 col-xs-5 resume-button"> 
+                <a href="./resume/ZachCalvert_2pg.pdf" download="ZachCalvert.pdf">PDF<br/>4 Page</a>
+              </div>
+              <div className="col-sm-4 col-sm-offset-2 col-xs-offset-1 col-xs-5 resume-button"> 
+                <a href="./resume/ZachCalvert_4pg.pdf"  download="ZachCalvert.pdf">PDF<br/>2 Page</a>
+              </div>
+              <div className="col-sm-4 col-xs-5 resume-button"> 
+                <a href="./resume/ZachCalvert_2pg.docx"  download="ZachCalvert.docx">MS DOC<br/>2 Page</a>
+              </div>
+              <div className="col-sm-4 col-sm-offset-2 col-xs-offset-1 col-xs-5 resume-button"> 
+                <a href="./resume/ZachCalvert_4pg.docx"  download="ZachCalvert.docx">MS DOC<br/>4 Page</a>
+              </div>
+              
             </div>
             <AboutMe/>
           </div>

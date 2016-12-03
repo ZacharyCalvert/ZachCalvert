353c3f3f07b7680eb94e9f8a4d27f1632bbe9bfd
Zach Calvert <ZachCalvert@gmail.com>
2016-12-02 22:20:54 -0600

    Upate to social page

--- src/Social.js
+++ src/Social.js
@@ -1,14 +1,51 @@
 import React, { Component } from 'react';
+import SubNav from './SubNav';
+
+var subNavEntries = [
+  {
+    page: 'github',
+    text: 'GitHub',
+    href: '#social'
+  },
+  {
+    page: 'linkedin',
+    text: 'LinkedIn',
+    href: '#social'
+  },
+  {
+    page: 'twitter',
+    text: 'Twitter',
+    href: '#social'
+  }
+];
 
 class Social extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = { selectedSubNav: "none" };
+    this.handleActionChange = this.handleActionChange.bind(this);
+  }
+
+  handleActionChange(updatedSelectedPage) {
+    if (updatedSelectedPage === 'twitter') {
+    	window.open('https://twitter.com/zacharycalvert');
+    } else if (updatedSelectedPage === 'linkedin') {
+		window.open('https://www.linkedin.com/in/zachary-calvert');
+    } else if (updatedSelectedPage === 'github') {
+    	window.open('https://github.com/ZacharyCalvert');
+    }
+  }
+
   render() {
+
     return (
-      <div className="container">
-      	<p>Hello to social</p>
+
+      <div>
+        <SubNav entries={subNavEntries} pageChanged={this.handleActionChange} selectedSubNav={this.state.selectedSubNav} />
+        <p>Pull from Twitter</p>
       </div>
     );
   }
 }
-
 export default Social;
--- src/SubNav.js
+++ src/SubNav.js
@@ -12,15 +12,6 @@ props:
     text: display text
     href: link
   }..]
-
-  <div id="navbar" className="navbar-collapse collapse navbar-right">
-            <ul className="nav navbar-nav">
-              <li className={this.props.selectedPage === "HOME" ? 'active' : 'deactive'} onClick={() => this.updateSelection('HOME')} ><a href="#home">Home</a></li>
-              <li className={this.props.selectedPage === "RESUME" ? 'active' : 'deactive'} onClick={() => this.updateSelection('RESUME')} ><a href="#resume">Resume</a></li>
-              <li className={this.props.selectedPage === "SOCIAL" ? 'active' : 'deactive'} onClick={() => this.updateSelection('SOCIAL')} ><a href="#social">Social</a></li>
-              <li className={this.props.selectedPage === "RECRUITERS" ? 'active' : 'deactive'} onClick={() => this.updateSelection('RECRUITERS')} ><a href="#recruiters">Recruiters</a></li>
-            </ul>
-          </div>
 */
 
   constructor(props) {

6f7216c3176c4a112e9a1c4566d557c7ad45b3a8
Zach Calvert <ZachCalvert@gmail.com>
2016-12-09 22:04:25 -0600

    Preparing resume for heroku

--- src/ResumeDownloads.js
+++ src/ResumeDownloads.js
@@ -9,16 +9,10 @@ class ResumeDownloads extends Component {
           <div className="row">
           	<div className="col-sm-8 col-xs-12">
               <div className="col-sm-4 col-xs-5 resume-button"> 
-                <a href="./resume/ZachCalvert_2pg.pdf" download="ZachCalvert.pdf">PDF<br/>4 Page</a>
+                <a href="./resume/ZachCalvert.pdf" download="ZachCalvert.pdf">PDF</a>
               </div>
               <div className="col-sm-4 col-sm-offset-2 col-xs-offset-1 col-xs-5 resume-button"> 
-                <a href="./resume/ZachCalvert_4pg.pdf"  download="ZachCalvert.pdf">PDF<br/>2 Page</a>
-              </div>
-              <div className="col-sm-4 col-xs-5 resume-button"> 
-                <a href="./resume/ZachCalvert_2pg.docx"  download="ZachCalvert.docx">MS DOC<br/>2 Page</a>
-              </div>
-              <div className="col-sm-4 col-sm-offset-2 col-xs-offset-1 col-xs-5 resume-button"> 
-                <a href="./resume/ZachCalvert_4pg.docx"  download="ZachCalvert.docx">MS DOC<br/>4 Page</a>
+                <a href="./resume/ZachCalvert.docx"  download="ZachCalvert.docx">MSDOC</a>
               </div>
               
             </div>

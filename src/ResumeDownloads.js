797b788ffc19671a4a4c7c7be899e8ad1371335a
Zach Calvert <ZachCalvert@gmail.com>
2016-12-09 23:04:08 -0600

    Download links done

--- src/ResumeDownloads.js
+++ src/ResumeDownloads.js
@@ -1,23 +1,33 @@
 import React, { Component } from 'react';
 import AboutMe from './AboutMe';
 
+var $ = require('jquery');
+
 class ResumeDownloads extends Component {
+
+  downloadClick(event) {
+    var fromClick = event.target.id;
+    var targetLink = '#' + fromClick + '_download';
+    $(targetLink)[0].click();
+  }
 
   render() {
     return (
         <div className="container">
           <div className="row">
           	<div className="col-sm-8 col-xs-12">
-              <div className="col-sm-4 col-xs-5 resume-button"> 
-                <a href="./resume/ZachCalvert.pdf" download="ZachCalvert.pdf">PDF</a>
+              <div id="pdf" onClick={this.downloadClick.bind(this)} className="col-sm-4 col-xs-5 resume-button"> 
+                PDF
               </div>
-              <div className="col-sm-4 col-sm-offset-2 col-xs-offset-1 col-xs-5 resume-button"> 
-                <a href="./resume/ZachCalvert.docx"  download="ZachCalvert.docx">MSDOC</a>
+              <div id="docx" onClick={this.downloadClick.bind(this)} className="col-sm-4 col-sm-offset-2 col-xs-offset-1 col-xs-5 resume-button"> 
+                MSDOC
               </div>
               
             </div>
             <AboutMe/>
           </div>
+          <a className="resume" download="ZachCalvert" href="./resume/ZachCalvert.pdf" id="pdf_download">PDF Link</a>
+          <a className="resume" download="ZachCalvert" href="./resume/ZachCalvert.docx" id="docx_download">MSDOC Link</a>
         </div>
     );
   }
